33f4febf875ce024b470097d5dc95be7e2b7be6b
Brian McIlwain <technicallyemployed@gmail.com>
2022-10-26 18:38:51 -0500

    Format

--- apps/api/wundergraph.config.ts
+++ apps/api/wundergraph.config.ts
@@ -1,10 +1,10 @@
 import {
-	Application,
-	configureWunderGraphApplication,
-	cors,
-	EnvironmentVariable,
-	introspect,
-	templates,
+  Application,
+  configureWunderGraphApplication,
+  cors,
+  EnvironmentVariable,
+  introspect,
+  templates
 } from '@wundergraph/sdk';
 import server from './wundergraph.server';
 import operations from './wundergraph.operations';
@@ -65,51 +65,52 @@ const graphQLAPI = introspect.graphql({
 });*/
 
 const countries = introspect.graphql({
-	apiNamespace: 'countries',
-	url: 'https://countries.trevorblades.com/',
+  apiNamespace: 'countries',
+  url: 'https://countries.trevorblades.com/'
 });
 
 const myApplication = new Application({
-	name: 'app',
-	apis: [
-		countries,
-		/*federatedApi,
+  name: 'app',
+  apis: [
+    countries
+    /*federatedApi,
         openAPI,
         graphQLAPI*/
-	],
+  ]
 });
 
 // configureWunderGraph emits the configuration
 configureWunderGraphApplication({
-	application: myApplication,
-	server,
-	operations,
-	codeGenerators: [
-		{
-			templates: [
-				// use all the typescript react templates to generate a client
-				...templates.typescript.all,
-				templates.typescript.operations,
-				templates.typescript.linkBuilder,
-			],
-			// create-react-app expects all code to be inside /src
-			// path: "../frontend/src/generated",
-		},
-	],
-	cors: {
-		...cors.allowAll,
-		allowedOrigins:
-			process.env.NODE_ENV === 'production'
-				? [
-						// change this before deploying to production to the actual domain where you're deploying your app
-						'http://localhost:3000',
-				  ]
-				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
-	},
-	dotGraphQLConfig: {
-		hasDotWunderGraphDirectory: false,
-	},
-	security: {
-		enableGraphQLEndpoint: process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined,
-	},
+  application: myApplication,
+  server,
+  operations,
+  codeGenerators: [
+    {
+      templates: [
+        // use all the typescript react templates to generate a client
+        ...templates.typescript.all,
+        templates.typescript.operations,
+        templates.typescript.linkBuilder
+      ]
+      // create-react-app expects all code to be inside /src
+      // path: "../frontend/src/generated",
+    }
+  ],
+  cors: {
+    ...cors.allowAll,
+    allowedOrigins:
+      process.env.NODE_ENV === 'production'
+        ? [
+            // change this before deploying to production to the actual domain where you're deploying your app
+            'http://localhost:3000'
+          ]
+        : ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')]
+  },
+  dotGraphQLConfig: {
+    hasDotWunderGraphDirectory: false
+  },
+  security: {
+    enableGraphQLEndpoint:
+      process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined
+  }
 });
