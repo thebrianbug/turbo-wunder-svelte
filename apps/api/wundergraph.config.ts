2c38223c1247b35e2aae3f05556af9e3688ffab4
Brian McIlwain <technicallyemployed@gmail.com>
2022-10-27 20:57:25 -0500

    Added generated-wundergraph package

--- apps/api/wundergraph.config.ts
+++ apps/api/wundergraph.config.ts
@@ -87,7 +87,7 @@ configureWunderGraphApplication({
   codeGenerators: [
     {
       templates: [templates.typescript.client],
-      path: './generated'
+      path: '../../packages/generated-wundergraph'
     }
   ],
   cors: {
