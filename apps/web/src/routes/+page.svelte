d61c1fa08018528951569324dba3cd1e391e6535
Brian McIlwain <technicallyemployed@gmail.com>
2022-10-27 21:11:24 -0500

    Try show output in svelte

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,12 +2,25 @@
   import { MyCounterButton } from 'ui';
   import { createClient } from 'generated-wundergraph';
 
-  export const client = createClient()
+  export const client = createClient();
 
-  console.log(client);
+  let d = client.query({
+    operationName: 'Countries'
+  });
+
+  console.log(d);
 </script>
 
 <h1>Web</h1>
 <MyCounterButton />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await d}
+  <span>Loading... please wait</span>
+{:then data}
+  <div>{data}</div>
+{:catch error}
+  <span>Something went wrong: {error.message} 🤕</span>
+  {console.error(error)}
+{/await}
