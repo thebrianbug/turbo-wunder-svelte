53b4b4928091fbd25d472f01f408c20e028df87a
Brian McIlwain <technicallyemployed@gmail.com>
2022-10-31 17:50:54 -0500

    Try making client type-safe

--- apps/web/src/routes/+page.ts
+++ apps/web/src/routes/+page.ts
@@ -1,9 +1,18 @@
+import type { OperationRequestOptions } from '@wundergraph/sdk/client';
 import { createClient } from 'generated-wundergraph';
+import type { Operations } from 'generated-wundergraph';
+
+type Queries = Extract<keyof Operations['queries'], string>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface QueryRequestOptions<OperationName extends Queries = Queries>
+  extends OperationRequestOptions<OperationName, Operations['queries'][OperationName]['input']> {}
 
 export async function load() {
   const client = createClient();
 
-  const response = await client.query({
+  // TODO: get types to generate for response
+  const response = await client.query<QueryRequestOptions>({
     operationName: 'Countries',
     input: {
       filter: {
