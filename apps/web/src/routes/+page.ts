d2d58647df88ed5b325e2603342016fb212ec7f5
Brian McIlwain <technicallyemployed@gmail.com>
2022-11-21 20:03:39 -0600

    Prefer auto-generated hooks

--- apps/web/src/routes/+page.ts
+++ apps/web/src/routes/+page.ts
@@ -1,13 +1,10 @@
 import type { Load } from '@sveltejs/kit';
 import { createClient } from 'generated-wundergraph';
-import type { Operations } from 'generated-wundergraph';
-import { createHooks } from '../lib/hooks';
 
 const client = createClient();
-const hooks = createHooks<Operations>(client);
 
 export const load: Load = async () => {
-  const response = await hooks.query({
+  const response = await client.query({
     operationName: 'Countries',
     input: {
       filter: {
